8fa13b08dc452acf0ebccd2b284af0697b77a64d
Mahadi Hasan <md.mahadihasan300@gmail.com>
2021-12-18 22:38:34 +0600

    manually author details save process in AstraDB done , added extra line to yml file to not to circular dependency

--- src/main/java/com/example/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/example/BetterReadsDataLoaderApplication.java
@@ -2,6 +2,9 @@ package com.example;
 
 import java.nio.file.Path;
 
+import com.example.author.Author;
+import com.example.author.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -9,15 +12,33 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 
 import connection.DataStaxAstraProperties;
+import org.springframework.context.annotation.Lazy;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterReadsDataLoaderApplication {
 
+	@Autowired AuthorRepository authorRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
 	}
-	
+
+
+	@PostConstruct
+	public void start(){
+		System.out.println("Application Started.....................");
+
+		Author author = new Author();
+		author.setId("123");
+		author.setName("Mahadi Hasan");
+		author.setPersonalName("Alfa Wisky");
+
+		authorRepository.save(author);
+	}
+
 	  /**
      * This is necessary to have the Spring Boot app use the Astra secure bundle 
      * to connect to the database
