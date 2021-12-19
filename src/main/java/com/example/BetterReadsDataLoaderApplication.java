c5408c3534caa27a4fcc284009cd722df31ccd92
Mahadi Hasan <md.mahadihasan300@gmail.com>
2021-12-19 23:35:30 +0600

    Author data automatically insert in DBfrom text file

--- src/main/java/com/example/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/example/BetterReadsDataLoaderApplication.java
@@ -1,10 +1,17 @@
 package com.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import com.example.author.Author;
 import com.example.author.AuthorRepository;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -26,17 +33,57 @@ public class BetterReadsDataLoaderApplication {
 		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
 	}
 
+	@Value("${datadump.location.author}")
+	private String authorDumpLocation;
+
+	@Value("${datadump.location.works}")
+	private String worksDumpLocaiton;
+
+	private void initAuthors(){
+		Path path = Paths.get(authorDumpLocation);
+		try(Stream<String> lines = Files.lines(path)){
+			//lines.limit(10).forEach(line->{
+			lines.forEach(line ->{
+				// Read and parse the line
+				String jsonString = line.substring(line.indexOf("{"));
+
+				try {
+					JSONObject jsonObject = new JSONObject(jsonString);
+					Author author = new Author();
+					author.setName(jsonObject.optString("name"));
+					author.setPersonalName(jsonObject.optString("personal_name"));
+					author.setId(jsonObject.optString("key").replace("/authors/", ""));
+
+					System.out.println("Saving author " + author.getName());
+					authorRepository.save(author);
+				}catch (JSONException e){
+					e.printStackTrace();
+				}
+					});
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+
+	}
+	private void initWorks(){
+
+	}
 
 	@PostConstruct
 	public void start(){
 		System.out.println("Application Started.....................");
 
-		Author author = new Author();
-		author.setId("123");
-		author.setName("Mahadi Hasan");
-		author.setPersonalName("Alfa Wisky");
+//		Author author = new Author();
+//		author.setId("123");
+//		author.setName("Mahadi Hasan");
+//		author.setPersonalName("Alfa Wisky");
+//
+//		authorRepository.save(author);
 
-		authorRepository.save(author);
+		System.out.println(worksDumpLocaiton);
+
+		initAuthors();
+		initWorks();
 	}
 
 	  /**
